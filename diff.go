16c86fca475ef1081ce6ddc0d5b61f9fc48553aa
gou3105 <gou3105@gmail.com>
2018-01-26 20:28:27 +0900

    working branch

--- diff.go
+++ diff.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"time"
+
+	"github.com/k0kubun/pp"
 )
 
 // DiffResult struct
@@ -13,11 +16,11 @@ type DiffResult struct {
 
 // Diff struct diff
 func Diff(before, after interface{}) error {
-	checkType(before)
+	checkStructType(before)
 
 	fmt.Println("")
 
-	checkType(after)
+	checkStructType(after)
 
 	return nil
 }
@@ -28,6 +31,7 @@ func convertStructToMap(data interface{}) map[string]interface{} {
 	elem := reflect.TypeOf(data)
 	size := elem.NumField()
 	fmt.Println(size)
+	fmt.Println(reflect.TypeOf(elem))
 
 	return res
 }
@@ -38,11 +42,22 @@ func convertPtrStructToMap(data interface{}) map[string]interface{} {
 	elem := reflect.ValueOf(data).Elem()
 	size := elem.NumField()
 	fmt.Println(size)
+	fmt.Println(reflect.TypeOf(elem))
+
+	for i := 0; i < size; i++ {
+		field := elem.Type().Field(i).Name
+		data := elem.Field(i)
+		// kind := data.Kind()
+		value := data.Interface()
+		res[field] = value
+	}
+
+	pp.Println(res)
 
 	return res
 }
 
-func checkType(data interface{}) map[string]interface{} {
+func checkStructType(data interface{}) map[string]interface{} {
 	switch reflect.TypeOf(data).Kind() {
 	case reflect.Struct:
 		return convertStructToMap(data)
@@ -59,9 +74,17 @@ type TestStruct struct {
 	D int16
 	E int32
 	F float32
+	G *string
+	H Sub
+}
+
+type Sub struct {
+	A string
 }
 
 func main() {
+	start := time.Now()
+	str := "ptr"
 	test1 := TestStruct{
 		"test1",
 		1,
@@ -71,6 +94,10 @@ func main() {
 		1,
 		1,
 		1.1,
+		&str,
+		Sub{
+			A: "test",
+		},
 	}
 	test2 := &TestStruct{
 		"test2",
@@ -81,6 +108,14 @@ func main() {
 		2,
 		2,
 		2.2,
+		&str,
+		Sub{
+			A: "test",
+		},
 	}
 	_ = Diff(test1, test2)
+
+	end := time.Now()
+
+	fmt.Printf("%v秒\n", end.Sub(start).Seconds())
 }
